6425bdb96e4e5c1ae2e59e6cf17542698424825b
ampatspell <ampatspell@gmail.com>
2019-09-03 20:25:51 +0300

    sign out

---
+++ editor/app/routes/session/delete.js
@@ -0,0 +1,23 @@
+import Route from '@ember/routing/route';
+import RequirementsMixin from '../-requirements';
+import alive from '../../utils/alive';
+
+export default Route.extend(RequirementsMixin, {
+
+  require: 'authenticated',
+
+  async beforeModel() {
+    await this._super(...arguments);
+    await this.signOut();
+  },
+
+  async signOut() {
+    await this.store.auth.signOut();
+    this.didSignOut();
+  },
+
+  didSignOut: alive(function() {
+    this.transitionTo('index');
+  }),
+
+});
