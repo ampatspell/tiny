89bfd7075491ef9ecc57dd6b1c31f66364289b80
ampatspell <ampatspell@gmail.com>
2019-09-04 14:24:11 +0300

    wip

--- editor/app/routes/wip.js
+++ editor/app/routes/wip.js
@@ -3,6 +3,13 @@ import RequirementsMixin from './-requirements';
 
 export default Route.extend(RequirementsMixin, {
 
-  require: 'authenticated'
+  require: 'authenticated',
+
+  async model() {
+    let project = this.store.models.create('project', { id: 'N01uqaf8XakLdobq4zFZ' });
+    await project.load();
+    let sprite = project.sprites.sprite('lkTlgQiLXXZ0Nj18deOy');
+    return sprite;
+  }
 
 });
