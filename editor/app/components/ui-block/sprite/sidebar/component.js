fd41415ed84c4ac23670d3a3acafe2f9c3d6d60c
ampatspell <ampatspell@gmail.com>
2019-09-05 17:20:18 +0300

    resize sidebar based on sprite size

--- editor/app/components/ui-block/sprite/sidebar/component.js
+++ editor/app/components/ui-block/sprite/sidebar/component.js
@@ -1,12 +1,21 @@
 import Component from '@ember/component';
 import { readOnly } from '@ember/object/computed';
+import { computed } from '@ember/object';
+import { htmlSafe } from '@ember/string';
 
 export default Component.extend({
   classNameBindings: [ ':ui-block-sprite-sidebar' ],
+  attributeBindings: [ 'style' ],
 
   state: null,
   sprite: readOnly('state.sprite'),
   frame: readOnly('state.frame'),
+
+  style: computed('sprite.size.width', function() {
+    let { sprite: { size: { width } } } = this;
+    width = Math.max(200, (width * 2) + 21);
+    return htmlSafe(`width: ${width}px`);
+  }).readOnly(),
 
   actions: {
     pixel(pixel) {
