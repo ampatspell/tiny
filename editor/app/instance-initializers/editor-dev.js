3142f3a1df626f5f893ec44dfb37acd5c9e43b4e
ampatspell <ampatspell@gmail.com>
2019-09-07 02:23:20 +0300

    cleanup

--- editor/app/instance-initializers/editor-dev.js
+++ editor/app/instance-initializers/editor-dev.js
@@ -5,47 +5,5 @@ export default {
   after: 'editor:store',
   initialize: app => { // eslint-disable-line no-unused-vars
     window.setGlobal = setGlobal;
-
-    let store = app.lookup('service:store');
-
-    window.save = async (path, width, height) => {
-      let sprite = store.doc(path).new({
-        name: 'Hello',
-        identifier: 'hello',
-        size: {
-          width,
-          height
-        }
-      });
-
-      let frame = index => {
-        let random = () => Math.floor(Math.random() * 3);
-        let arr = [];
-        for(let i = 0; i < (width * height); i++) {
-          arr.push(random());
-        }
-        let bytes = new Uint8Array(arr);
-        return store.doc(`${path}/frames/${index}`).new({
-          index,
-          bytes
-        });
-      }
-
-      let frames = [
-        frame(0),
-        frame(1),
-        frame(2),
-        frame(3),
-        frame(4)
-      ];
-
-      await Promise.all([
-        sprite.save(),
-        ...frames.map(frame => frame.save())
-      ]);
-
-      return { sprite, frames };
-    }
-
   }
 };
