d33a1d97f288b298a1925626cb247d6fbc33714a
Arnis Vuskans <ampatspell@gmail.com>
2019-09-10 01:24:44 +0300

    drag start for sprite in sidebar

--- editor/app/components/ui-block/world/sidebar/sprites/sprite/component.js
+++ editor/app/components/ui-block/world/sidebar/sprites/sprite/component.js
@@ -1,13 +1,32 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   classNameBindings: [ ':sprite' ],
+  attributeBindings: [ 'draggable' ],
+
+  draggable: true,
 
   state: null,
   sprite: null,
 
+  size: computed('state.pixel', 'sprite.size', function() {
+    let { sprite: { size }, state: { pixel } } = this;
+    let calc = key => size[key] * pixel;
+    return {
+      width: calc('width'),
+      height: calc('height')
+    };
+  }).readOnly(),
+
   click() {
     this.select();
+  },
+
+  dragStart(e) {
+    let dragImage = this.element.querySelector('.drag-image');
+    e.dataTransfer.setData('text/data', { type: 'sprite', id: this.sprite.id });
+    e.dataTransfer.setDragImage(dragImage, 0, 0);
   }
 
 });
