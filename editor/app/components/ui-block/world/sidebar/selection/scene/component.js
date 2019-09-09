6f0d34abe07968a05a23aacc9d616d2abf8b62a0
Arnis Vuskans <ampatspell@gmail.com>
2019-09-09 19:54:55 +0300

    add scene background color

--- editor/app/components/ui-block/world/sidebar/selection/scene/component.js
+++ editor/app/components/ui-block/world/sidebar/selection/scene/component.js
@@ -1,5 +1,21 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import { readOnly } from '@ember/object/computed';
+
+const backgrounds = [
+  { value: 'black', label: 'Black' },
+  { value: 'white', label: 'White' },
+  { value: 'transparent', label: 'None' }
+];
+
+const choice = (arrayKey, valueKey) => computed(`arrayKey.@each.value`, valueKey, function() {
+  let array = this.get(arrayKey);
+  if(!array) {
+    return;
+  }
+  let value = this.get(valueKey);
+  return array.findBy('value', value);
+}).readOnly();
 
 export default Component.extend({
   classNameBindings: [ ':ui-block-world-sidebar-selection-scene' ],
@@ -7,9 +23,15 @@ export default Component.extend({
   scene: readOnly('state.selection'),
   locked: readOnly('state.locked'),
 
+  backgrounds,
+  background: choice('backgrounds', 'scene.background'),
+
   actions: {
     update(key, value) {
       this.scene.update({ [key]: value });
+    },
+    background({ value: background }) {
+      this.scene.update({ background });
     },
     createLayer() {
       this.state.createLayer();
