b79ce1d92787ee0650b9968bdc707332ac76a06c
Arnis Vuskans <ampatspell@gmail.com>
2019-09-09 21:48:53 +0300

    clip layer

--- editor/app/components/ui-konva/world/scene/layers/layer/component.js
+++ editor/app/components/ui-konva/world/scene/layers/layer/component.js
@@ -1,7 +1,27 @@
 import Node from '../../../../-node';
+import { computed } from '@ember/object';
+
+const observe = [ 'scene.size' ];
 
 export default Node.extend({
 
-  nodeClassName: 'group'
+  nodeClassName: 'group',
+  observe,
+
+  clipFunc: computed('scene.size', 'pixel', function() {
+    let { scene: { size }, pixel } = this;
+    let width = size.width * pixel;
+    let height = size.height * pixel;
+    return ctx => {
+      ctx.rect(0, 0, width, height);
+    };
+  }).readOnly(),
+
+  props: computed('clipFunc', function() {
+    let { clipFunc } = this;
+    return {
+      clipFunc
+    }
+  }).readOnly(),
 
 });
