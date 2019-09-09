7de5ee710e08e485d492367f12cfa2c73050e830
Arnis Vuskans <ampatspell@gmail.com>
2019-09-10 02:11:10 +0300

    rename scene/tools as scene/pixel

--- editor/app/components/ui-block/scene/pixel/component.js
+++ editor/app/components/ui-block/scene/pixel/component.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNameBindings: [ ':ui-block-scene-tools' ],
+  classNameBindings: [ ':ui-block-scene-pixel' ],
 
   pixel: null,
 
