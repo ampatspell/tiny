33290fbf8e1d6a6795fd9608c2dcc88b13d017ac
Arnis Vuskans <ampatspell@gmail.com>
2019-09-11 00:20:13 +0300

    cleanup

--- editor/app/components/ui-block/world/inspector/selection/layer/grid/component.js
+++ editor/app/components/ui-block/world/inspector/selection/layer/grid/component.js
@@ -1,32 +1,4 @@
 import Component from '../-layer';
-import { computed } from '@ember/object';
 
 export default Component.extend({
-
-  actions: {
-    createFillNode() {
-      this.state.createNode({
-        type: 'fill',
-        position: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 8,
-          height: 8
-        },
-        color: 'black'
-      });
-    },
-    createSpriteNode() {
-      this.state.createNode({
-        type: 'sprite',
-        position: {
-          x: 0,
-          y: 0,
-        }
-      });
-    },
-  }
-
 });
