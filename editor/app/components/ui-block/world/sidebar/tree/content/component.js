d80e09d12c804b036335ba6e1bc13581ef7ba47e
Arnis Vuskans <ampatspell@gmail.com>
2019-09-10 03:16:42 +0300

    Update component.js

--- editor/app/components/ui-block/world/sidebar/tree/content/component.js
+++ editor/app/components/ui-block/world/sidebar/tree/content/component.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNameBindings: [ ':ui-block-world-sidebar-tree-row' ],
+  classNameBindings: [ ':ui-block-world-sidebar-tree-row', 'model.locked:locked', 'model.hidden:hidden' ],
 
   actions: {
     lock() {
