af2b59cd152a31c2f9baa72ac050486617343095
ampatspell <ampatspell@gmail.com>
2019-09-11 23:11:27 +0300

    extract state/sprite

--- editor/app/components/ui-route/projects/project/sprites/sprite/index/component.js
+++ editor/app/components/ui-route/projects/project/sprites/sprite/index/component.js
@@ -1,63 +1,13 @@
 import Component from '@ember/component';
 import { readOnly } from '@ember/object/computed';
-import { delta, current } from 'editor/utils/computed';
 import KeyboardMixin from 'editor/utils/keyboard';
 import { model } from 'ember-cli-zuglet/lifecycle';
-import alive from 'editor/utils/alive';
 
 export default Component.extend(KeyboardMixin, {
   classNameBindings: [ ':ui-route-projects-project-sprites-sprite-index' ],
 
   sprite: readOnly('model.sprite'),
-
-  // TODO: move to models/state/sprite
-  state: model().owner('sprite').inline({
-
-    editor: null,
-
-    frames: readOnly('sprite.frames'),
-
-    prev: delta('frames', 'frame', -1),
-    next: delta('frames', 'frame', +1),
-
-    frame: current('frames', 0),
-
-    pixel: 16,
-
-    onEditorCreated(editor) {
-      this.setProperties({ editor });
-    },
-
-    update: alive(function(props) {
-      this.setProperties(props);
-    }),
-
-    center() {
-      this.editor.center();
-    },
-
-    onLeft() {
-      let frame = this.prev;
-      frame && this.update({ frame });
-    },
-
-    onRight() {
-      let frame = this.next;
-      frame && this.update({ frame });
-    },
-
-    async duplicate() {
-      let frame = await this.frame.duplicate();
-      this.update({ frame });
-    },
-
-    async delete() {
-      let { frame, prev } = this;
-      this.update({ frame: prev });
-      await frame.delete();
-    }
-
-  }).mapping(({ sprite }) => ({ sprite })),
+  state: model().owner('sprite').named('state/sprite').mapping(({ sprite }) => ({ sprite })),
 
   actions: {
     created(editor) {
---
+++ editor/app/models/state/sprite.js
@@ -0,0 +1,52 @@
+import EmberObject from '@ember/object';
+import { readOnly } from '@ember/object/computed';
+import { delta, current } from 'editor/utils/computed';
+import alive from 'editor/utils/alive';
+
+export default EmberObject.extend({
+
+  editor: null,
+
+  frames: readOnly('sprite.frames'),
+
+  prev: delta('frames', 'frame', -1),
+  next: delta('frames', 'frame', +1),
+
+  frame: current('frames', 0),
+
+  pixel: 16,
+
+  onEditorCreated(editor) {
+    this.setProperties({ editor });
+  },
+
+  update: alive(function(props) {
+    this.setProperties(props);
+  }),
+
+  center() {
+    this.editor.center();
+  },
+
+  onLeft() {
+    let frame = this.prev;
+    frame && this.update({ frame });
+  },
+
+  onRight() {
+    let frame = this.next;
+    frame && this.update({ frame });
+  },
+
+  async duplicate() {
+    let frame = await this.frame.duplicate();
+    this.update({ frame });
+  },
+
+  async delete() {
+    let { frame, prev } = this;
+    this.update({ frame: prev });
+    await frame.delete();
+  }
+
+});
