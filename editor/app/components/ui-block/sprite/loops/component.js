455941dea535bc2ee491bee81674452fb9b228db
ampatspell <ampatspell@gmail.com>
2019-09-12 03:26:45 +0300

    loops/loop component

--- editor/app/components/ui-block/sprite/loops/component.js
+++ editor/app/components/ui-block/sprite/loops/component.js
@@ -1,7 +1,12 @@
 import Component from '@ember/component';
+import { readOnly } from '@ember/object/computed';
 
 export default Component.extend({
   classNameBindings: [ ':ui-block-sprite-loops' ],
+
+  state: null,
+  sprite: readOnly('state.sprite'),
+  loops: readOnly('sprite.loops'),
 
   actions: {
     createLoop() {
---
+++ editor/app/components/ui-block/sprite/loops/loop/component.js
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNameBindings: [ ':loop' ],
+
+  state: null,
+  loops: null,
+  loop: null,
+
+});
