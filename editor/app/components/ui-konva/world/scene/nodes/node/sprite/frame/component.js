9e49a29c6f0edc1895a1ad5b4fce0146551c6a19
ampatspell <ampatspell@gmail.com>
2019-09-13 01:11:37 +0300

    render selected frame for node/frame

--- editor/app/components/ui-konva/world/scene/nodes/node/sprite/frame/component.js
+++ editor/app/components/ui-konva/world/scene/nodes/node/sprite/frame/component.js
@@ -1,4 +1,8 @@
 import Component from '../-sprite';
+import { readOnly } from '@ember/object/computed';
 
 export default Component.extend({
+
+  rendered: readOnly('layerNode.selected.preview.rendered'),
+
 });
