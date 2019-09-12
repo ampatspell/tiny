d66bc61355740a733ce8f6bbc87b0441e7a48ccc
ampatspell <ampatspell@gmail.com>
2019-09-12 23:40:16 +0300

    move common stuff to sprite/base

---
+++ editor/app/components/ui-block/world/inspector/selection/node/sprite/base/component.js
@@ -0,0 +1,12 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  tagName: '',
+
+  actions: {
+    update(...args) {
+      this.update(...args);
+    }
+  }
+
+});
