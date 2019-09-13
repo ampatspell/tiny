e49b01b9cffaff7d1ddabb068a001bd22c4abdd4
Arnis Vuskans <ampatspell@gmail.com>
2019-09-13 23:10:45 +0300

    export

--- editor/firebase/functions/src/services/export.js
+++ editor/firebase/functions/src/services/export.js
@@ -1,4 +1,5 @@
 let { properties } = require('../util/lazy');
+let { pick } = require('../util/object');
 
 class ExportService {
 
@@ -6,8 +7,147 @@ class ExportService {
     this.app = app;
   }
 
+  async _exportProject(project) {
+    let data = project.data();
+    if(!data) {
+      return;
+    }
+    return Object.assign({ id: project.id }, pick(data, [ 'title' ]));
+  }
+
+  async _exportSpriteFrame(frame) {
+    let data = frame.data();
+    if(!data) {
+      return;
+    }
+
+    let bytes = data.bytes || null;
+    if(bytes) {
+      bytes = [ ...bytes ];
+    }
+
+    return Object.assign({ id: frame.id, bytes }, pick(data, [ 'identifier', 'index' ]));
+  }
+
+  async _exportSpriteLoop(loop) {
+    let data = loop.data();
+    if(!data) {
+      return;
+    }
+    return Object.assign({ id: loop.id }, pick(data, [ 'identifier', 'frames' ]));
+  }
+
+  async _exportSpriteFrames(frames) {
+    return await Promise.all(frames.docs.map(snapshot => this._exportSpriteFrame(snapshot)));
+  }
+
+  async _exportSpriteLoops(loops) {
+    return await Promise.all(loops.docs.map(snapshot => this._exportSpriteLoop(snapshot)));
+  }
+
+  async _exportSprite(sprite) {
+    let data = sprite.data();
+    if(!data) {
+      return;
+    }
+
+    let [ frames, loops ] = await Promise.all([
+      this._exportSpriteFrames(await sprite.ref.collection('frames').orderBy('index', 'asc').get()),
+      this._exportSpriteLoops(await sprite.ref.collection('loops').get()),
+    ]);
+
+    return Object.assign(pick(data, [ 'identifier', 'size' ]), { id: sprite.id, frames, loops });
+  }
+
+  async _exportSprites(sprites) {
+    return await Promise.all(sprites.docs.map(snapshot => this._exportSprite(snapshot)));
+  }
+
+  async _exportSceneLayerNode(node) {
+    let data = node.data();
+    if(!data) {
+      return;
+    }
+
+    return Object.assign(pick(data, [ 'index', 'type', 'sprite', 'frame', 'position', 'alignment', 'flip', 'loop', 'color' ]), { id: node.id });
+  }
+
+  async _exportSceneLayerNodes(nodes) {
+    return await Promise.all(nodes.docs.map(snapshot => this._exportSceneLayerNode(snapshot)));
+  }
+
+  async _exportSceneLayer(layer) {
+    let data = layer.data();
+    if(!data) {
+      return;
+    }
+
+    let nodes = await this._exportSceneLayerNodes(await layer.ref.collection('nodes').orderBy('index', 'asc').get());
+
+    return Object.assign(pick(data, [ 'identifier', 'type', 'index', 'grid' ]), { id: layer.id, nodes });
+  }
+
+  async _exportSceneLayers(layers) {
+    return await Promise.all(layers.docs.map(snapshot => this._exportSceneLayer(snapshot)));
+  }
+
+  async _exportScene(scene) {
+    let data = scene.data();
+    if(!data) {
+      return;
+    }
+
+    let layers = await this._exportSceneLayers(await scene.ref.collection('layers').orderBy('index', 'asc').get());
+
+    return Object.assign(pick(data, [ 'identifier', 'background', 'name', 'size' ]), { id: scene.id, layers });
+  }
+
+  async _exportScenes(scenes) {
+    return await Promise.all(scenes.docs.map(snapshot => this._exportScene(snapshot)));
+  }
+
+  async _exportWorld(world) {
+    let data = world.data();
+    if(!data) {
+      return;
+    }
+
+    let [ sprites, scenes ] = await Promise.all([
+      this._exportSprites(await world.ref.parent.parent.collection('sprites').get()),
+      this._exportScenes(await world.ref.collection('scenes').get())
+    ]);
+
+    return Object.assign(pick(data, [ 'name' ]), { id: world.id, sprites, scenes });
+  }
+
+  async exportWorld(worldSnapshot) {
+    let [ project, world ] = await Promise.all([
+      this._exportProject(await worldSnapshot.ref.parent.parent.get()),
+      this._exportWorld(worldSnapshot)
+    ]);
+
+    return {
+      project,
+      world
+    };
+  }
+
+  async usingToken(token) {
+    let snapshot = await this.app.firestore.collectionGroup('worlds').where('token', '==', token).get();
+
+    if(snapshot.size === 0) {
+      throw new Error(`Word not found for token '${token}'`);
+    }
+
+    if(snapshot.size > 1) {
+      throw new Error(`Multiple worlds fortoken '${token}' found`);
+    }
+
+    return await this.exportWorld(snapshot.docs[0]);
+  }
+
 }
 
-properties(ExportService, [ ], name => require(`./export/${name}`));
+properties(ExportService, [], name => require(`./export/${name}`));
 
 module.exports = app => new ExportService(app);
--- editor/firebase/functions/test/services/export-test.js
+++ editor/firebase/functions/test/services/export-test.js
@@ -5,11 +5,199 @@ describe('services / export', () => {
   setup(this);
 
   beforeEach(() => {
+    require('events').EventEmitter.defaultMaxListeners = 15;
+
     this.export = this.app.services.export;
+    this.doc = path => this.admin.firestore.doc(path);
+    this.documents = {
+      'projects/bug': {
+        title: 'The Bug'
+      },
+      'projects/bug/sprites/one': {
+        identifier: 'weirdo',
+        size: {
+          width: 16,
+          height: 16
+        }
+      },
+      'projects/bug/sprites/one/frames/1': {
+        index: 1,
+        bytes: new Uint8Array([ 1, 2, 3 ])
+      },
+      'projects/bug/sprites/one/frames/2': {
+        identifier: '2',
+        index: 2,
+        bytes: new Uint8Array([ 2, 3, 4 ])
+      },
+      'projects/bug/sprites/one/loops/wink': {
+        identifier: 'wink',
+        frames: [ '1', '2' ]
+      },
+      'projects/bug/worlds/earth': {
+        token: 'the-token',
+        name: 'Earth'
+      },
+      'projects/bug/worlds/earth/scenes/intro': {
+        background: 'black',
+        identifier: 'intro',
+        name: 'Earth',
+        size: {
+          width: 128,
+          height: 64
+        }
+      },
+      'projects/bug/worlds/earth/scenes/intro/layers/background': {
+        identifier: 'background',
+        type: 'grid',
+        index: 0,
+        grid: {
+          width: 8,
+          height: 8
+        }
+      },
+      'projects/bug/worlds/earth/scenes/intro/layers/background/nodes/1': {
+        index: 0,
+        type: 'sprite/frame',
+        sprite: "weirdo",
+        frame: "2",
+        position: {
+          x: 104,
+          y: 40
+        },
+        alignment: {
+          horizontal: 'left',
+          vertical: 'top'
+        },
+        flip: {
+          horizontal: false,
+          vertical: false
+        }
+      },
+      'projects/bug/worlds/earth/scenes/intro/layers/char': {
+        identifier: 'char',
+        type: 'pixel',
+        index: 1
+      },
+      'projects/bug/worlds/earth/scenes/intro/layers/char/nodes/1': {
+        index: 0,
+        type: 'sprite/loop',
+        sprite: "weirdo",
+        loop: 'wink',
+        position: {
+          x: 104,
+          y: 40
+        },
+        alignment: {
+          horizontal: 'left',
+          vertical: 'top'
+        },
+        flip: {
+          horizontal: false,
+          vertical: false
+        }
+      },
+      'projects/bug/worlds/earth/scenes/intro/layers/char/nodes/2': {
+        index: 1,
+        type: 'fill',
+        color: 'white',
+        identifier: 'box',
+        position: {
+          x: 104,
+          y: 40
+        },
+        size: {
+          width: 8,
+          height: 8
+        }
+      }
+    }
+    this.insert = async () => {
+      let batch = this.admin.firestore.batch();
+      let docs = this.documents;
+      Object.keys(docs).forEach(path => batch.set(this.doc(path), docs[path]));
+      await batch.commit();
+    }
   });
 
   it('hello', async () => {
-    console.log(this.export);
+    await this.insert();
+    let json = await this.export.usingToken('the-token');
+    assert.deepEqual(json, {
+      project: { id: 'bug', title: 'The Bug' },
+      world: {
+        id: 'earth',
+        name: 'Earth',
+        sprites: [
+          {
+            id: 'one',
+            identifier: 'weirdo',
+            size: { height: 16, width: 16 },
+            frames: [
+              { id: '1', bytes: [ 1, 2, 3 ], index: 1 },
+              { id: '2', bytes: [ 2, 3, 4 ], identifier: '2', index: 2 }
+            ],
+            loops: [
+              { id: 'wink', identifier: 'wink', frames: [ '1', '2' ] }
+            ]
+          }
+        ],
+        scenes: [
+          {
+            id: 'intro',
+            identifier: 'intro',
+            background: 'black',
+            name: 'Earth',
+            size: { height: 64, width: 128 },
+            layers: [
+              {
+                id: 'background',
+                identifier: 'background',
+                type: 'grid',
+                index: 0,
+                grid: { height: 8, width: 8 },
+                nodes: [
+                  {
+                    id: '1',
+                    index: 0,
+                    type: 'sprite/frame',
+                    sprite: 'weirdo',
+                    frame: '2',
+                    position: { x: 104, y: 40 },
+                    alignment: { vertical: 'top', horizontal: 'left' },
+                    flip: { horizontal: false, vertical: false }
+                  }
+                ]
+              },
+              {
+                id: 'char',
+                identifier: 'char',
+                type: 'pixel',
+                index: 1,
+                nodes: [
+                  {
+                    id: '1',
+                    index: 0,
+                    type: 'sprite/loop',
+                    sprite: 'weirdo',
+                    position: { y: 40, x: 104 },
+                    alignment: { vertical: 'top', horizontal: 'left' },
+                    flip: { horizontal: false, vertical: false },
+                    loop: 'wink',
+                  },
+                  {
+                    id: '2',
+                    index: 1,
+                    type: 'fill',
+                    position: { y: 40, x: 104 },
+                    color: 'white'
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    });
   });
 
 });
