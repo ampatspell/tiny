5b0e6924e744cc1a99ed99016e98d25e9cd5a796
ampatspell <ampatspell@gmail.com>
2019-09-14 08:14:43 +0300

    Update scene.js

--- player/tiny/generate/scene.js
+++ player/tiny/generate/scene.js
@@ -3,8 +3,15 @@ module.exports = async (runtime, scene) => {
   let file = [
     '#pragma once',
     '#include <avr/pgmspace.h>',
+    '',
+    `// Scene: "${scene.name}" ${scene.identifier}`,
     ''
   ];
+
+  // struct Scene01 {
+  //   static uint8_t width = 100;
+  // };
+
 
   await runtime.write(`scene_${scene.identifier}.h`, file.join('\n'));
 
