99924ae4eaf42659eb07b16c20ae5b757341d67e
ampatspell <ampatspell@gmail.com>
2019-09-17 17:30:10 +0300

    number of scenes

--- player/src/main.cpp
+++ player/src/main.cpp
@@ -3,12 +3,16 @@
 #include <generated/scenes.h>
 #include <tiny/scene/scene.h>
 
-Scene *scene;
-uint8_t idx;
+Scene *scene = nullptr;
+uint8_t idx = 0;
+bool info = false;
 
 void setup() {
   arduboy.begin();
   arduboy.setFrameRate(24);
+  if(Tiny::Scenes::getNumberOfScenes() == 0) {
+    return;
+  }
   scene = Tiny::Scenes::instantiate(0);
 }
 
@@ -19,14 +23,32 @@ void loop() {
 
   arduboy.pollButtons();
 
-  if(arduboy.everyXFrames(48)) {
-    idx = idx == 0 ? 1 : 0;
-    scene = Tiny::Scenes::instantiate(idx);
+  if(arduboy.justPressed(UP_BUTTON)) {
+    if(idx > 0) {
+      idx = idx - 1;
+      scene = Tiny::Scenes::instantiate(idx);
+    }
+  } else if(arduboy.justPressed(DOWN_BUTTON)) {
+    if(idx + 1 < Tiny::Scenes::getNumberOfScenes()) {
+      idx = idx + 1;
+      scene = Tiny::Scenes::instantiate(idx);
+    }
   }
 
-  scene->draw();
+  if(arduboy.justPressed(A_BUTTON)) {
+    info = true;
+  } else if(arduboy.justReleased(A_BUTTON)) {
+    info = false;
+  }
 
- Tiny::snprintf(0, 0, 20, "scene:%u", idx);
+  if(scene) {
+    scene->draw();
+    if(info) {
+    Tiny::snprintf(0, 0, 20, "Scene #%u", idx);
+    }
+  } else {
+    Tiny::snprintf(0, 0, 20, "No Scenes");
+  }
 
   arduboy.display(true);
 }
