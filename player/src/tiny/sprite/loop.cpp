33a5da8ff7e66da103f9f25bf9c3ece9318ab033
ampatspell <ampatspell@gmail.com>
2019-09-22 02:43:39 +0300

    cleanup

--- player/src/tiny/sprite/loop.cpp
+++ player/src/tiny/sprite/loop.cpp
@@ -2,18 +2,19 @@
 #include <tiny/sprite/loop.h>
 #include <tiny/sprite/sprite.h>
 
-Loop::Loop(const uint8_t *_definition, Sprite *_sprite): definition(_definition), sprite(_sprite) {
+Loop::Loop(const uint8_t *_definition, Sprite *_sprite) :
+    definition(_definition), sprite(_sprite) {
 }
 
 uint8_t Loop::next(uint8_t index) {
   uint8_t total = pgm_read_byte(definition);
-  if(index + 1 < total) {
-     return index + 1;
+  if (index + 1 < total) {
+    return index + 1;
   }
   return 0;
 }
 
 void Loop::draw(uint8_t x, uint8_t y, uint8_t index) {
-  uint8_t frame = pgm_read_byte(definition + 1 + index);
+  uint8_t frame = pgm_read_byte(definition + sizeof(uint8_t) + index);
   sprite->draw(x, y, frame);
 }
