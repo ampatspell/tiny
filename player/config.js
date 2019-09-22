220a817c489182320ac6874582f0bc2ccaa2124b
ampatspell <ampatspell@gmail.com>
2019-09-22 18:56:59 +0300

    split out config

---
+++ player/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: 'https://europe-west2-dev-tiny.cloudfunctions.net/export-world',
+  token: 'Ny7qX8WIN5zW05mpPwqqWk43'
+}
--- player/tiny/config.js
+++ player/tiny/config.js
@@ -1,13 +1,11 @@
+const parent = require('../config');
 const path = require('path');
-
 const root = path.join(__dirname, '..');
 
-module.exports =  {
+module.exports =  Object.assign({
   target: path.join(root, 'src/generated'),
   cache:  {
     payload: path.join(root, 'payload-cache.json'),
     readable: path.join(root, 'payload-readable.json')
-  },
-  url: 'https://europe-west2-dev-tiny.cloudfunctions.net/export-world',
-  token: 'Ny7qX8WIN5zW05mpPwqqWk43'
-};
+  }
+}, parent);
