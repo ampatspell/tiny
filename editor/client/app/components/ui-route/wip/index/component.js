6d1d05c537ba6e872a67813bce09947128fb1b11
Arnis Vuskans <ampatspell@gmail.com>
2019-09-24 01:21:49 +0300

    wip

--- editor/client/app/components/ui-route/wip/index/component.js
+++ editor/client/app/components/ui-route/wip/index/component.js
@@ -1,9 +1,5 @@
 import Component from '@ember/component';
-import { readOnly } from '@ember/object/computed';
 
 export default Component.extend({
-  classNameBindings: [ ':ui-route-wip-index' ],
-
-  frame: readOnly('model.frames.firstObject'),
-
+  classNameBindings: [ ':ui-route-wip-index' ]
 });
--- editor/client/app/routes/wip.js
+++ editor/client/app/routes/wip.js
@@ -6,11 +6,6 @@ export default Route.extend(RequirementsMixin, {
   require: 'authenticated',
 
   async model() {
-    let project = this.store.models.create('project', { id: 'N01uqaf8XakLdobq4zFZ' });
-    await project.load();
-    let sprite = project.sprites.sprite('lkTlgQiLXXZ0Nj18deOy');
-    await sprite.load();
-    return sprite;
   }
 
 });
