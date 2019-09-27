ca813d79795e66f2c914dce6634e7bb45bb8d310
Arnis Vuskans <ampatspell@gmail.com>
2019-09-27 23:16:55 +0300

    add dialogs from bain

---
+++ editor/client/app/components/ui-block/dialogs/component.js
@@ -0,0 +1,41 @@
+import Component from '@ember/component';
+import layout from './template';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  layout,
+  tagName: '',
+
+  dialogs: service(),
+  label: null,
+
+  model: computed('dialogs.models.firstObject', function() {
+    let model = this.dialogs.models.firstObject;
+    if(!model || model.presenter !== this) {
+      return;
+    }
+    return model;
+  }).readOnly(),
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.dialogs.registerPresenter(this);
+  },
+
+  willDestroyElement() {
+    this.dialogs.unregisterPresenter(this);
+    this._super(...arguments);
+  },
+
+  actions: {
+    cancel() {
+      this.model.cancel();
+    }
+  },
+
+  toStringExtension() {
+    return this.label;
+  }
+
+});
--- editor/client/app/components/ui-dialog/component.js
+++ editor/client/app/components/ui-dialog/component.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNameBindings: [ ':ui-route-wip-index' ]
+  classNameBindings: [ ':ui-dialog' ]
 });
