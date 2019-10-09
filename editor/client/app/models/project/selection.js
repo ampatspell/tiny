402d110139f678a6b354f17f1c9c4aa883e5f977
ampatspell <ampatspell@gmail.com>
2019-10-10 02:27:43 +0300

    Update selection.js

--- editor/client/app/models/project/selection.js
+++ editor/client/app/models/project/selection.js
@@ -4,18 +4,25 @@ export default EmberObject.extend({
 
   project: null,
 
+  _normalize(model) {
+    return model || this.project;
+  },
+
   model: computed({
     get() {
-      return this._model || this.project;
+      return this._normalize(this._model);
     },
     set(key, value) {
-      value = value || this.project;
+      value = this._normalize(value);
       this._model = value;
       return value;
     }
   }),
 
   select(model) {
+    if(this._normalize(model) === this.model) {
+      return;
+    }
     this.setProperties({ model });
     setGlobal({ selection: model });
   }
