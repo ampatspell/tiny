2c1600366a5e4bdae1b750684791c8b3faf2cb26
ampatspell <ampatspell@gmail.com>
2019-10-09 05:47:21 +0300

    item for background

--- editor/client/app/components/ui-block/project/inspector/tabs/scene/main/component.js
+++ editor/client/app/components/ui-block/project/inspector/tabs/scene/main/component.js
@@ -1,28 +1,5 @@
 import Component from '../../-component';
-import { computed } from '@ember/object';
-
-const backgrounds = [
-  { value: 'black', label: 'Black' },
-  { value: 'white', label: 'White' },
-  { value: 'transparent', label: 'None' }
-];
-
-const background = () => computed(`backgrounds.@each.value`, 'model.background', function() {
-  let array = this.backgrounds;
-  let value = this.model.background;
-  return array.findBy('value', value);
-}).readOnly();
 
 export default Component.extend({
-  classNameBindings: [ ':ui-block-project-inspector-tabs-scene-main' ],
-
-  backgrounds,
-  background: background(),
-
-  actions: {
-    background(background) {
-      this.update('background', background.value);
-    }
-  }
-
+  classNameBindings: [ ':ui-block-project-inspector-tabs-scene-main' ]
 });
