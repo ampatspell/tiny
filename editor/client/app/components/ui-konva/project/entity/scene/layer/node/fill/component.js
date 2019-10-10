3dfed5bb7f6c0af6c685431d95d1f320f60e350f
Arnis Vuskans <ampatspell@gmail.com>
2019-10-10 15:05:06 +0300

    fill node ui

--- editor/client/app/components/ui-konva/project/entity/scene/layer/node/fill/component.js
+++ editor/client/app/components/ui-konva/project/entity/scene/layer/node/fill/component.js
@@ -1,4 +1,34 @@
 import Node from '../-node';
+import { computed } from '@ember/object';
+import { readOnly } from '@ember/object/computed';
+
+const mapping = {
+  'red':   'rgba(255, 102, 97, 0.4)',
+  'green': 'rgba(97, 255, 123, 0.5)',
+  'black': '#000',
+  'white': '#fff'
+};
+
+const observe = [ '_frame', 'fill' ];
 
 export default Node.extend({
+
+  nodeClassName: 'rect',
+  observe,
+
+  color: readOnly('model.color'),
+
+  fill: computed('model.color', function() {
+    return mapping[this.model.color];
+  }).readOnly(),
+
+  props: computed('_frame', 'fill', function() {
+    let { _frame: { width, height }, fill } = this;
+    return  {
+      width,
+      height,
+      fill
+    }
+  }).readOnly()
+
 });
