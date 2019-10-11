8816dadf61e6497a2922d4e0ae28b7cf89e8ab28
Arnis Vuskans <ampatspell@gmail.com>
2019-10-11 23:32:50 +0300

    add new scene

--- editor/client/app/models/project/scenes.js
+++ editor/client/app/models/project/scenes.js
@@ -1,6 +1,32 @@
 import filteredEntities from './-filtered-entities';
+import { assign } from '@ember/polyfills';
+import { readOnly } from '@ember/object/computed';
 
 const Scenes = filteredEntities('scene');
 
 export default Scenes.extend({
+
+  project: readOnly('model'),
+
+  async didCreate() {
+  },
+
+  async create(opts) {
+    let {
+      identifier,
+      position,
+      size,
+      background
+    } = assign({
+      identifier: 'new-scene',
+      position: { x: 4, y: 6 },
+      size: { width: 128, height: 64 },
+      background: 'white'
+    }, opts);
+
+    let model = await this.createModel({ type: 'scene', identifier, position, size, background });
+    await this.didCreate(model);
+    return model;
+  },
+
 });
