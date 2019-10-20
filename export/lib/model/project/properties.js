5632a3b756c539609e636ace3517b227138ba301
Arnis Vuskans <ampatspell@gmail.com>
2019-10-20 09:45:42 +0300

    remove properties.inspect

--- export/lib/model/project/properties.js
+++ export/lib/model/project/properties.js
@@ -3,14 +3,7 @@ class Properties {
   constructor(parent, json) {
     Object.defineProperty(this, 'parent', { value: parent });
     Object.defineProperty(this, 'json', { value: json });
-  }
-
-  inspect() {
-    let values = '';
-    if(this.any) {
-      values = ` ${this.keys.join(', ')} `;
-    }
-    return `Properties {${values}}`;
+    this.keys = Object.keys(json);
   }
 
   get any() {
@@ -19,10 +12,6 @@ class Properties {
 
   get all() {
     return this.json;
-  }
-
-  get keys() {
-    return Object.keys(this.json);
   }
 
   get(key, def) {
