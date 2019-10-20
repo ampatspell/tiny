66f274e8ff8ff5500a0ea792d581ae57c60a2685
Arnis Vuskans <ampatspell@gmail.com>
2019-10-20 07:45:57 +0300

    array util

--- export/lib/util/array.js
+++ export/lib/util/array.js
@@ -1,17 +1,35 @@
-const sortBy = (array, key) => {
+const get = (object, key) => {
+  let keys = key.split('.');
+  let len = keys.length;
+  let i = 0;
+  while(i < len) {
+    object = object[keys[i]];
+    i++;
+  }
+  return object;
+}
+
+const sortBy = (array, key, direction='asc') => {
+
   let compare = (a, b) => {
-    let av = a[key];
-    let bv = b[key];
+    let av = get(a, key);
+    let bv = get(b, key);
+
     if(av < bv) {
       return -1;
     }
     if(av > bv) {
       return +1;
     }
+
     return 0;
   }
-  return array.slice().sort(compare);
-}
+
+  return array.slice().sort((a, b) => {
+    let value = compare(a, b);
+    return direction === 'desc' ? value * -1 : value;
+  });
+};
 
 module.exports = {
   sortBy
