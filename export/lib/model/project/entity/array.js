b6dfd97668996e83da19cc3c9689642fbfc91788
ampatspell <ampatspell@gmail.com>
2020-03-09 02:03:11 +0200

    Update array.js

--- export/lib/model/project/entity/array.js
+++ export/lib/model/project/entity/array.js
@@ -8,11 +8,20 @@ module.exports = array => {
     }
     Object.defineProperty(array, name, { value });
   };
+
+  let addGetter = (name, get) => {
+    if(array.hasOwnProperty(name)) {
+      return;
+    }
+    Object.defineProperty(array, name, { get });
+  }
   
   let addFind = (name, key) => addFunction(name, arg => array.find(model => model[key] === arg ));
+  let addFilter = (name, cb) => addGetter(name, () => array.filter(cb));
 
   addFind('byIdentifier', 'identifier');
   addFind('byId', 'id');
+  addFilter('identified', model => !!model.identifier);
   addFunction('toHexStrings', () => pixels.toHexStrings([ ...array ]));
 
   return array;
