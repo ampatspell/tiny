66203835f196ca1c031d4111ad7f547834ce4284
Arnis Vuskans <ampatspell@gmail.com>
2026-08-13 02:01:50 +0300

    Update handle.ts

--- src/lib/server/handle.ts
+++ src/lib/server/handle.ts
@@ -8,6 +8,7 @@ import type { DB } from './database/schema.js';
 
 export const createHandle = ({ storageRoot }: { storageRoot: string }): Handle => {
   return async ({ event, resolve }) => {
+    console.log('storageRoot:', storageRoot);
     const { db, files, storage } = await createServices({
       base: storageRoot,
     });
