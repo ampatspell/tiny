160376c03f6b92c1aa0a6ff4f5830c96a22bdcaf
Arnis Vuskans <ampatspell@gmail.com>
2026-08-13 02:03:14 +0300

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -2,5 +2,5 @@ import * as v from 'valibot';
 import { defineEnvVars } from '@sveltejs/kit/env';
 
 export const variables = defineEnvVars({
-  STORAGE_ROOT: { public: false, static: true, schema: v.string() },
+  STORAGE_ROOT: { public: false, static: false, schema: v.string() },
 });
