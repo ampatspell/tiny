d74b17e98a7c75b8d5f94d8cb142387e88283cf8
Arnis Vuskans <ampatspell@gmail.com>
2026-08-19 19:13:16 +0300

    Update +page.svelte

--- src/routes/dev/+page.svelte
+++ src/routes/dev/+page.svelte
@@ -1,22 +1,5 @@
 <script lang="ts">
-  import Button from '$lib/components/button/button.svelte';
-  import { pickFile } from '$lib/utils/files.svelte.js';
-
-  const onClick = async () => {
-    let res = await pickFile({
-      accept: ['image/png', 'image/jpeg'],
-    });
-
-    console.log(res);
-  };
+  import Placeholder from '$lib/components/layout/placeholder/placeholder.svelte';
 </script>
 
-<div class="dev">
-  <Button label="Pick" {onClick} />
-</div>
-
-<style lang="scss">
-  .dev {
-    padding: 10px;
-  }
-</style>
+<Placeholder />
