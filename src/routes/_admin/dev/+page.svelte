80f9990194d5780b8d8d725d3a835688d4235177
Arnis Vuskans <ampatspell@gmail.com>
2026-08-26 17:46:43 +0300

    Update +page.svelte

--- src/routes/_admin/dev/+page.svelte
+++ src/routes/_admin/dev/+page.svelte
@@ -1,5 +1,45 @@
-<script lang="ts">
-  import Placeholder from '$lib/tiny/layout/placeholder/placeholder.svelte';
+<script module lang="ts">
 </script>
 
-<Placeholder />
+<script lang="ts">
+  import Button from '$lib/tiny/button/button.svelte';
+
+  let input = $state<HTMLInputElement>();
+
+  let oninput = (e: Event) => {
+    let target = e.target as HTMLInputElement;
+    console.log(target.value);
+  };
+</script>
+
+<div class="page">
+  <div class="picker">
+    <input bind:this={input} class="input" type="color" {oninput} />
+    <Button label="Select color" onClick={() => input?.click()} />
+  </div>
+</div>
+
+<style lang="scss">
+  .page {
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+
+    .picker {
+      display: flex;
+      width: min-content;
+      position: relative;
+      > .input {
+        position: absolute;
+        bottom: -5px;
+        left: 0;
+        width: 0;
+        height: 0;
+        padding: 0;
+        border: none;
+        opacity: 0;
+        pointer-events: none;
+      }
+    }
+  }
+</style>
