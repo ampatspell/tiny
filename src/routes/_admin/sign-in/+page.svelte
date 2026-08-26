934279c8aa99b0821b3cb1e6337ed4423d9e0745
Arnis Vuskans <ampatspell@gmail.com>
2026-08-27 02:06:11 +0300

    generic form

--- src/routes/_admin/sign-in/+page.svelte
+++ src/routes/_admin/sign-in/+page.svelte
@@ -1,27 +1,8 @@
 <script lang="ts">
+  import Auth from '$lib/playground/auth/auth.svelte';
   import { useSignIn } from '$lib/playground/auth/auth.svelte.js';
-  import Button from '$lib/tiny/button/button.svelte';
-  import Center from '$lib/tiny/center.svelte';
-  import Actions from '$lib/tiny/form/actions.svelte';
-  import Row from '$lib/tiny/form/content/row.svelte';
-  import Form from '$lib/tiny/form/form.svelte';
-  import Header from '$lib/tiny/form/header.svelte';
-  import Input from '$lib/tiny/properties/input.svelte';
 
   let model = useSignIn();
 </script>
 
-<Center>
-  <Form size="small">
-    <Header title="Sign in" />
-    <Row>
-      <Input property={model.email} />
-    </Row>
-    <Row>
-      <Input property={model.password} type="password" />
-    </Row>
-    <Actions>
-      <Button label="Sign in" onClick={model.perform} />
-    </Actions>
-  </Form>
-</Center>
+<Auth {model} title="Sign in" />
--- src/routes/_admin/sign-up/+page.svelte
+++ src/routes/_admin/sign-up/+page.svelte
@@ -1,27 +1,8 @@
 <script lang="ts">
+  import Auth from '$lib/playground/auth/auth.svelte';
   import { useSignUp } from '$lib/playground/auth/auth.svelte.js';
-  import Button from '$lib/tiny/button/button.svelte';
-  import Center from '$lib/tiny/center.svelte';
-  import Actions from '$lib/tiny/form/actions.svelte';
-  import Row from '$lib/tiny/form/content/row.svelte';
-  import Form from '$lib/tiny/form/form.svelte';
-  import Header from '$lib/tiny/form/header.svelte';
-  import Input from '$lib/tiny/properties/input.svelte';
 
   let model = useSignUp();
 </script>
 
-<Center>
-  <Form size="small">
-    <Header title="Sign up" />
-    <Row>
-      <Input property={model.email} />
-    </Row>
-    <Row>
-      <Input property={model.password} type="password" />
-    </Row>
-    <Actions>
-      <Button label="Sign up" onClick={model.perform} />
-    </Actions>
-  </Form>
-</Center>
+<Auth {model} title="Sign up" />
