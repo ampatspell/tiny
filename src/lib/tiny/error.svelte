d28cd3e0681477972d7d5bce4c499fe9d64369b7
Arnis Vuskans <ampatspell@gmail.com>
2026-08-28 00:51:40 +0300

    error page

---
+++ src/lib/tiny/error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { page } from '$app/state';
+  import TablerBalloon from '$lib/tiny/icons/tabler--balloon.svelte';
+  import Placeholder from '$lib/tiny/placeholder.svelte';
+  import Tiny from '$lib/tiny/tiny.svelte';
+  import { isTruthy } from '$lib/tiny/utils/array.js';
+
+  let status = $derived(page.status);
+  let error = $derived(page.error?.message ?? 'Unknown error');
+  let label = $derived.by(() => {
+    return [status !== 200 && status, error].filter(isTruthy).join(' ');
+  });
+</script>
+
+<Tiny>
+  <Placeholder icon={TablerBalloon} {label} />
+</Tiny>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,17 +1,5 @@
 <script lang="ts">
-  import { page } from '$app/state';
-  import TablerBalloon from '$lib/tiny/icons/tabler--balloon.svelte';
-  import Placeholder from '$lib/tiny/placeholder.svelte';
-  import Tiny from '$lib/tiny/tiny.svelte';
-  import { isTruthy } from '$lib/tiny/utils/array.js';
-
-  let status = $derived(page.status);
-  let error = $derived(page.error?.message ?? 'Unknown error');
-  let label = $derived.by(() => {
-    return [status !== 200 && status, error].filter(isTruthy).join(' ');
-  });
+  import Error from '$lib/tiny/error.svelte';
 </script>
 
-<Tiny>
-  <Placeholder icon={TablerBalloon} {label} />
-</Tiny>
+<Error />
