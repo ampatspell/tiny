7f22c7c07fa2032a8c9e8cb9ee0859ffe8853442
Arnis Vuskans <ampatspell@gmail.com>
2026-09-01 00:42:34 +0300

    move fontsource to deps

--- src/tests/files.spec.ts
+++ src/tests/files.spec.ts
@@ -1,9 +1,9 @@
 import { describe, expect, it } from 'vitest';
 import { readTestFileAsFile, withServices } from './helpers/utils.ts';
-import type { Files } from '$lib/tiny/server/files/files.js';
-import type { Database } from '$lib/tiny/server/database/database.js';
-import type { Storage } from '$lib/tiny/server/storage/storage.js';
-import type { DB } from '$lib/tiny/server/database/schema.js';
+import type { Files } from '#lib/tiny/server/files/files.js';
+import type { Database } from '#lib/tiny/server/database/database.js';
+import type { Storage } from '#lib/tiny/server/storage/storage.js';
+import type { DB } from '#lib/tiny/server/database/schema.js';
 
 const withFiles = async <T>(cb: (opts: { files: Files; db: Database<DB>; storage: Storage }) => Promise<T>) => {
   return await withServices(async (services) => {
