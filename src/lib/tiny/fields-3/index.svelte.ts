5b3690e9e2cca4b2734e4ab2cc8731e6db113bc0
Arnis Vuskans <ampatspell@gmail.com>
2026-09-08 00:38:23 +0300

    so much simpler

--- src/lib/tiny/fields-3/index.svelte.ts
+++ src/lib/tiny/fields-3/index.svelte.ts
@@ -1,5 +1,7 @@
+import { sentenceCase } from 'text-sentence-case';
 import { getter, options, type OptionsInput } from '../utils/options.svelte.ts';
 import type { NumberKey, StringKey } from '../utils/utils.ts';
+import { FieldsContext } from './context.svelte.ts';
 
 export type Data = Record<string, unknown>;
 
@@ -8,28 +10,58 @@ export type FieldOptions<FD extends FieldDefinition> = {
   data: Data;
 };
 
-export class Field<T = unknown, FD extends FieldDefinition = FieldDefinition> {
+export abstract class Field<T = unknown, FD extends FieldDefinition = FieldDefinition> {
   private readonly opts: FieldOptions<FD>;
-  private readonly data = $derived.by(() => this.opts.data);
+  private readonly parent = $derived.by(() => this.opts.data);
+
   readonly definition = $derived.by(() => this.opts.definition);
+  readonly context = $derived.by(() => this.definition.context);
   readonly key = $derived.by(() => this.definition.key);
-  readonly external = $derived.by(() => this.data[this.key] as T);
+  readonly data = $derived.by(() => this.parent[this.key] as T);
+  readonly isTouched = $derived(this.context.isTouched);
+  readonly label = $derived.by(() => this.definition.label);
+  readonly description = $derived.by(() => this.definition.description);
+  readonly isValid = $derived.by(() => !this.error);
 
   constructor(opts: OptionsInput<FieldOptions<FD>>) {
     this.opts = options(opts);
   }
+
+  abstract readonly isRequired: boolean;
+  abstract readonly isDirty: boolean;
+  abstract readonly error: string | undefined;
+  abstract readonly serialized: unknown;
+  abstract rollback(): void;
 }
 
-export class StringField extends Field<string, StringFieldDefinition> {}
-export class NumberField extends Field<number, NumberFieldDefinition> {}
+export class ValueField<T, D extends FieldDefinition> extends Field<T, D> {
+  readonly isRequired = false;
+  readonly isDirty = false;
+  readonly error = undefined;
+  readonly serialized = $derived(this.data);
+  rollback() {}
+}
+
+export class StringField extends ValueField<string, StringFieldDefinition> {}
+export class NumberField extends ValueField<number, NumberFieldDefinition> {}
+
+type BaseFieldDefinitionOptions = {
+  label?: string;
+  description?: string;
+};
 
 export type FieldDefinitionOptions = {
+  context: FieldsContext;
   key: string;
-};
+} & BaseFieldDefinitionOptions;
 
 export abstract class FieldDefinition {
   private readonly opts: FieldDefinitionOptions;
+
+  readonly context = $derived.by(() => this.opts.context);
   readonly key = $derived.by(() => this.opts.key);
+  readonly label = $derived.by(() => this.opts.label ?? sentenceCase(this.key));
+  readonly description = $derived.by(() => this.opts.description);
 
   constructor(opts: OptionsInput<FieldDefinitionOptions>) {
     this.opts = options(opts);
@@ -64,6 +96,12 @@ export type InferFieldsFromDefinitionRecord<R extends Data> = {
 
 export type InferFieldFromDefinition<T> = T extends FieldDefinition ? ReturnType<T['field']> : T;
 
+export type InferSerializedFromFieldsRecord<R extends Data> = {
+  [K in keyof R]: InferSerializedFromField<R[K]>;
+};
+
+export type InferSerializedFromField<T> = T extends Field ? T['serialized'] : T;
+
 export type FieldsOptions<D extends Data, F extends Factory<D>> = {
   data: D;
   factory: F;
@@ -80,6 +118,7 @@ export class Fields<
   }
 
   readonly data = $derived.by(() => this.opts.data);
+  readonly context = $derived.by(() => this.opts.factory.context);
   private readonly definition = $derived.by(() => this.opts.factory.record);
 
   readonly record = $derived.by(() => {
@@ -93,17 +132,32 @@ export class Fields<
         record[key] = definition;
       }
     }
-    console.log(record);
     return record as InferFieldsFromDefinitionRecord<typeof definitions>;
+  });
+
+  readonly serialized = $derived.by(() => {
+    const record = this.record;
+    const serialized: Record<string, unknown> = {};
+    for (const key in record) {
+      const field = record[key];
+      if (field instanceof Field) {
+        serialized[key] = field.serialized;
+      } else {
+        serialized[key] = field;
+      }
+    }
+    return serialized as InferSerializedFromFieldsRecord<typeof record>;
   });
 }
 
 export type FactoryOptions<D extends Data = Data, R extends Data = Data> = {
+  context: FieldsContext;
   cb: (factory: Factory<D>) => R;
 };
 
 export class Factory<D extends Data = Data, R extends Data = Data> {
   private readonly opts: FactoryOptions<D, R>;
+  readonly context = $derived.by(() => this.opts.context);
 
   constructor(opts: OptionsInput<FactoryOptions<D, R>>) {
     this.opts = options(opts);
@@ -111,21 +165,31 @@ export class Factory<D extends Data = Data, R extends Data = Data> {
 
   readonly record = $derived.by(() => this.opts.cb(this));
 
-  readonly string = <K extends StringKey<D>>(key: K) => {
-    return new StringFieldDefinition({ key });
+  private get base() {
+    return {
+      context: getter(() => this.context),
+    };
+  }
+
+  readonly string = <K extends StringKey<D>>(key: K, opts?: OptionsInput<BaseFieldDefinitionOptions>) => {
+    return new StringFieldDefinition({ key, ...this.base, ...opts });
   };
 
-  readonly number = <K extends NumberKey<D>>(key: K) => {
-    return new NumberFieldDefinition({ key });
+  readonly number = <K extends NumberKey<D>>(key: K, opts?: OptionsInput<BaseFieldDefinitionOptions>) => {
+    return new NumberFieldDefinition({ key, ...this.base, ...opts });
   };
 }
 
 export type FieldsDefinitionOptions<D extends Data = Data> = {
   data: D;
+  context?: FieldsContext;
 };
 
 export class FieldsDefinition<D extends Data = Data> {
   private readonly opts: FieldsDefinitionOptions<D>;
+
+  readonly data = $derived.by(() => this.opts.data);
+  readonly context = $derived.by(() => this.opts.context ?? new FieldsContext());
 
   constructor(opts: OptionsInput<FieldsDefinitionOptions<D>>) {
     this.opts = options(opts);
@@ -134,6 +198,7 @@ export class FieldsDefinition<D extends Data = Data> {
   factory<R extends Data>(cb: (factory: Factory<D>) => R) {
     return new Factory({
       cb,
+      context: getter(() => this.context),
     });
   }
 
--- src/routes/(tiny)/_admin/(nav)/dev/+page.svelte
+++ src/routes/(tiny)/_admin/(nav)/dev/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { FieldsDefinition } from '#lib/tiny/fields-3/index.svelte.js';
+  import Json from '#lib/tiny/json.svelte';
 
   let definition = new FieldsDefinition({ data: { name: 'duck', hamsters: 5 } });
   let fields = definition.define(({ string, number }) => {
@@ -12,5 +13,7 @@
   let ff = fields.record;
 </script>
 
-{ff.hamsters.external}
-{ff.name.external}
+{ff.hamsters.data}
+{ff.name.data}
+
+<Json data={fields.serialized} />
