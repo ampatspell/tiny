1fcf657bbe3c633e5e4bb51a72558b3a148e1742
Arnis Vuskans <ampatspell@gmail.com>
2026-09-08 02:01:36 +0300

    Update array-field.svelte.ts

--- src/lib/tiny/fields-3/array-field.svelte.ts
+++ src/lib/tiny/fields-3/array-field.svelte.ts
@@ -3,7 +3,7 @@ import { FieldDefinition } from './field-definition.svelte.ts';
 import { Field } from './field.svelte.ts';
 import type { Data } from './types.svelte.ts';
 
-export class ArrayField<T extends Data> extends Field<T[], ArrayFieldDefinition<T>> {
+export class ArrayField<T extends Data = Data> extends Field<T[], ArrayFieldDefinition<T>> {
   isRequired = false;
   isDirty = false;
   error = undefined;
@@ -11,7 +11,7 @@ export class ArrayField<T extends Data> extends Field<T[], ArrayFieldDefinition<
   rollback() {}
 }
 
-export class ArrayFieldDefinition<T extends Data> extends FieldDefinition {
+export class ArrayFieldDefinition<T extends Data = Data> extends FieldDefinition {
   field(opts: OptionsInput<{ data: Data }>): ArrayField<T> {
     return new ArrayField<T>({
       definition: this,
