9126be97b17db4c2d2bd446a72783c28d8a9edc5
Arnis Vuskans <ampatspell@gmail.com>
2026-09-08 18:55:11 +0300

    Update file-field.svelte.ts

--- src/lib/tiny/fields-3/file-field.svelte.ts
+++ src/lib/tiny/fields-3/file-field.svelte.ts
@@ -4,19 +4,19 @@ import File from './file.svelte';
 import type { Data } from './types.svelte.ts';
 import { ValueField, ValueFieldDefinition, type ValueFieldDefinitionOptions } from './value-field.svelte.ts';
 
-type T = UniversalFile | undefined;
+type F = UniversalFile | undefined;
 
-export class FileField extends ValueField<T, FileFieldDefinition> {
+export class FileField extends ValueField<F, FileFieldDefinition> {
   readonly accept = $derived(this.definition.accept);
   readonly onSelected = (next: LocalFile | undefined) => this.update(next);
   protected editor = File;
 }
 
-export type FileFieldDefinitionOptions = ValueFieldDefinitionOptions<T> & {
+export type FileFieldDefinitionOptions = ValueFieldDefinitionOptions<F> & {
   accept?: string[];
 };
 
-export class FileFieldDefinition extends ValueFieldDefinition<T, FileFieldDefinitionOptions> {
+export class FileFieldDefinition extends ValueFieldDefinition<F, FileFieldDefinitionOptions> {
   readonly accept = $derived(this.opts.accept);
 
   field(opts: OptionsInput<{ data: Data }>): FileField {
