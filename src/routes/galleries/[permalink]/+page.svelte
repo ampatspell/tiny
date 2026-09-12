79cfc92a6003f3141248b311d95b1d105eceef88
Arnis Vuskans <ampatspell@gmail.com>
2026-09-12 12:22:27 +0300

    Update +page.svelte

--- src/routes/galleries/[permalink]/+page.svelte
+++ src/routes/galleries/[permalink]/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { getGalleryByPermalink } from '#lib/playground/galleries/galleries.remote.js';
   import { useFiles } from '#lib/tiny/files.svelte.js';
-  import { url } from '#lib/tiny/utils/style.js';
   import { page } from '$app/state';
   import { innerHeight, innerWidth } from 'svelte/reactivity/window';
 
@@ -16,43 +15,87 @@
 </script>
 
 <div class={['page', `type-${type}`]}>
-  <div class="header">
-    <div class="title">{gallery.name}</div>
-  </div>
-  <div class="files">
+  <div class="blocks">
+    <div class="block">
+      <div class="description">
+        <div class="title">{gallery.name}</div>
+      </div>
+    </div>
     {#each gallery.files.filter((file) => file.file) as file (file.id)}
-      <div class="file" style:--url={url(files.resolve({ id: file.file!.id, variant: '2048x2048' }))}></div>
+      <div class="block">
+        <div class="description">{file.name}</div>
+        <div class="details">
+          <img
+            class="file"
+            draggable="false"
+            alt={file.name}
+            src={files.resolve({ id: file.file!.id, variant: '2048x2048' })}
+          />
+        </div>
+      </div>
     {/each}
   </div>
 </div>
 
 <style lang="scss">
   .page {
-    &.type-landscape {
-      --gap: 50px;
-      --height: calc(100vh);
-    }
-    &.type-portrait {
-      --gap: 0px;
-      --height: calc(100vw);
-    }
-    flex: 1;
     display: flex;
     flex-direction: column;
-    gap: 50px;
-    padding: 50px 0;
-    > .header {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
+    > .blocks {
+      > .block {
+        > .description {
+          font-size: var(--tiny-font-size-small);
+          > .title {
+            font-weight: 600;
+            font-size: var(--tiny-font-size);
+          }
+        }
+      }
     }
-    > .files {
-      display: flex;
-      flex-direction: column;
-      gap: var(--gap);
-      > .file {
-        height: var(--height);
-        background: var(--url) center center / contain no-repeat;
+    &.type-landscape {
+      > .blocks {
+        display: flex;
+        flex-direction: column;
+        > .block {
+          display: flex;
+          flex-direction: row;
+          padding: 50px;
+          > .description {
+            width: 300px;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-end;
+          }
+          > .details {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            > .file {
+              max-height: calc(100vh - 100px);
+            }
+          }
+        }
+      }
+    }
+    &.type-portrait {
+      padding: 20px 0;
+      > .blocks {
+        display: flex;
+        flex-direction: column;
+        gap: 25px;
+        > .block {
+          display: flex;
+          flex-direction: column-reverse;
+          gap: 10px;
+          > .description {
+            padding: 0 10px;
+          }
+          > .details {
+            > .file {
+              max-height: calc(100vh - 20px);
+            }
+          }
+        }
       }
     }
   }
