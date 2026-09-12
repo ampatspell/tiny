d518b9f5806f2172b3647cec8518b1c4a6412ea2
Arnis Vuskans <ampatspell@gmail.com>
2026-09-12 15:10:57 +0300

    Update +page.svelte

--- src/routes/galleries/[permalink]/+page.svelte
+++ src/routes/galleries/[permalink]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { getGalleryByPermalink } from '#lib/playground/galleries/galleries.remote.js';
   import { useFiles } from '#lib/tiny/files.svelte.js';
+  import { isTruthy, sortedBy } from '#lib/tiny/utils/array.js';
   import { page } from '$app/state';
   import { innerHeight, innerWidth } from 'svelte/reactivity/window';
 
@@ -17,21 +18,43 @@
 
   let getCurrent = () => {
     let wy = window.scrollY;
-    return blocks.find((block) => {
-      let rect = block.getBoundingClientRect();
-      let ry = rect.y + wy;
-      return ry > wy;
-    });
+    let wh = window.innerHeight;
+
+    let visible = sortedBy(
+      blocks
+        .map((el) => {
+          let rect = el.getBoundingClientRect();
+          let ry = rect.y + wy;
+          let rh = rect.height;
+          let a = ry + rh > wy;
+          let b = ry < wy + wh;
+          if (a && b) {
+            let top = Math.max(ry, wy);
+            let bottom = Math.min(ry + rh, wy + wh);
+            let visible = bottom - top;
+            let fraction = visible / rh;
+            return {
+              el,
+              height: rh,
+              fraction,
+            };
+          }
+        })
+        .filter(isTruthy),
+      { value: (v) => v.fraction, direction: 'desc' },
+    );
+
+    console.log(visible.map((v) => `${v.el.innerText} ${v.fraction}`));
+
+    return visible[0]?.el;
   };
 
   let scrollIntoView = (e: Event, cb: (current: HTMLElement) => Element | null) => {
     e.preventDefault();
-    let current = getCurrent();
-    if (current) {
-      let next = cb(current);
-      if (next instanceof HTMLElement) {
-        next.scrollIntoView({ behavior: 'smooth', block: 'center' });
-      }
+    let visible = getCurrent();
+    let next = cb(visible);
+    if (next instanceof HTMLElement) {
+      next.scrollIntoView({ behavior: 'smooth', block: 'center' });
     }
   };
 
@@ -50,21 +73,27 @@
 
 <div class={['page', `type-${type}`]}>
   <div class="blocks">
-    <div class="block">
-      <div class="description">
-        <div class="title">{gallery.name}</div>
-      </div>
-    </div>
     {#each gallery.files.filter((file) => file.file) as file, i (file.id)}
       <div class="block" bind:this={blocks[i]}>
-        <div class="description">{file.name}</div>
-        <div class="details">
-          <img
-            class="file"
-            draggable="false"
-            alt={file.name}
-            src={files.resolve({ id: file.file!.id, variant: '2048x2048' })}
-          />
+        <div class="content">
+          <div class="description">
+            {#if i === 0}
+              <div class="main">
+                <div class="title">{gallery.name}</div>
+              </div>
+            {/if}
+            <div class="footer">
+              {file.name}
+            </div>
+          </div>
+          <div class="details">
+            <img
+              class="file"
+              draggable="false"
+              alt={file.name}
+              src={files.resolve({ id: file.file!.id, variant: '2048x2048' })}
+            />
+          </div>
         </div>
       </div>
     {/each}
@@ -77,19 +106,7 @@
     flex-direction: column;
     > .blocks {
       > .block {
-        > .description {
-          font-size: var(--tiny-font-size-small);
-          > .title {
-            font-weight: 600;
-            font-size: var(--tiny-font-size);
-          }
-        }
-        > .details {
-          > .file {
-            object-fit: contain;
-            object-position: right;
-          }
-        }
+        font-size: var(--tiny-font-size-small);
       }
     }
     &.type-landscape {
@@ -98,41 +115,36 @@
         flex-direction: column;
         > .block {
           display: flex;
-          flex-direction: row;
-          padding: 50px;
-          > .description {
-            width: 300px;
+          flex-direction: column;
+          justify-content: center;
+          height: 100vh;
+          > .content {
+            max-height: 100%;
+            padding: 50px;
             display: flex;
-            flex-direction: column;
-            justify-content: flex-end;
-          }
-          > .details {
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            > .file {
-              max-height: calc(100vh - 50px);
+            flex-direction: row;
+            > .description {
+              display: flex;
+              flex-direction: column;
+              justify-content: flex-end;
+              width: 300px;
+              > .main {
+                flex: 1;
+                > .title {
+                  font-size: var(--tiny-font-size);
+                  font-weight: 700;
+                }
+              }
             }
-          }
-        }
-      }
-    }
-    &.type-portrait {
-      padding: 25px 0;
-      > .blocks {
-        display: flex;
-        flex-direction: column;
-        gap: 25px;
-        > .block {
-          display: flex;
-          flex-direction: column-reverse;
-          gap: 10px;
-          > .description {
-            padding: 0 15px;
-          }
-          > .details {
-            > .file {
-              max-height: calc(100vh - 30px);
+            > .details {
+              flex: 1;
+              height: 100%;
+              > .file {
+                width: 100%;
+                height: 100%;
+                object-fit: contain;
+                object-position: right;
+              }
             }
           }
         }
