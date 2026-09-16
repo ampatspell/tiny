890747d01a6a6a37e1ead431bde7d7298f48ef58
Arnis Vuskans <ampatspell@gmail.com>
2026-09-16 22:54:43 +0300

    Update +page.svelte

--- src/routes/(tiny)/_admin/(nav)/dev/+page.svelte
+++ src/routes/(tiny)/_admin/(nav)/dev/+page.svelte
@@ -1,56 +1,120 @@
 <script lang="ts">
-  import type { ItemData } from '#lib/tiny/dropdown/basic/items.svelte';
-  import { withDataFields } from '#lib/tiny/fields/index.svelte.js';
-  import Fields from '#lib/tiny/form/content/fields.svelte';
-  import Form from '#lib/tiny/form/form.svelte';
-  import { getter } from '#lib/tiny/utils/options.svelte.js';
+  import { isTruthy } from '#lib/tiny/utils/array.js';
+  import { elementContainsEventTarget } from '#lib/tiny/utils/dom.js';
+  import { getter, options, type OptionsInput } from '#lib/tiny/utils/options.svelte.js';
+  import { images } from '#lib/tiny/utils/utils.js';
+  import type { Attachment } from 'svelte/attachments';
+  import { on } from 'svelte/events';
 
-  type Model = {
-    id: string;
-    label: string;
-  } & ItemData;
-
-  let items = $state<Model[]>([
-    {
-      id: 'one',
-      label: 'Duck',
-    },
-    {
-      id: 'two',
-      label: 'Hamster',
-    },
-    {
-      id: 'three',
-      label: 'Zeeba',
-      state: 'critical',
-    },
-  ]);
-
-  let fields = withDataFields({
-    data: {
-      role: 'admin',
-    },
-  }).define(({ dropdown }) => {
-    return {
-      role: dropdown('role', {
-        items: getter(() => items),
-        identifier: 'id',
-      }),
+  const fileDrop = (model: FileDropModel): Attachment<HTMLElement> => {
+    const fileItemsFromEvent = (e: DragEvent) => {
+      let items = e.dataTransfer?.items ?? [];
+      let all = [...items].filter((item) => item.kind === 'file') ?? [];
+      let accepted = all.filter((item) => model.accept.includes(item.type));
+      return { all, accepted };
     };
+
+    return (element) => {
+      let onDragOver = (e: DragEvent) => {
+        const { all, accepted } = fileItemsFromEvent(e);
+        if (all.length > 0) {
+          e.preventDefault();
+          if (e.dataTransfer) {
+            if (accepted.length > 0) {
+              e.dataTransfer.dropEffect = 'copy';
+              model.setOver(true);
+            } else {
+              e.dataTransfer.dropEffect = 'none';
+            }
+          }
+        }
+      };
+
+      let onWindowDragOver = (e: DragEvent) => {
+        const { all } = fileItemsFromEvent(e);
+        if (all.length > 0) {
+          e.preventDefault();
+          if (!elementContainsEventTarget(element, e)) {
+            if (e.dataTransfer) {
+              e.dataTransfer.dropEffect = 'none';
+            }
+          }
+        }
+      };
+
+      let onDragLeave = () => {
+        model.setOver(false);
+      };
+
+      let onDrop = (e: DragEvent) => {
+        let { all, accepted } = fileItemsFromEvent(e);
+        if (all.length > 0) {
+          e.preventDefault();
+          if (accepted.length > 0) {
+            let files = accepted.map((item) => item.getAsFile()).filter(isTruthy);
+            model.onDrop(files);
+          }
+        }
+        model.setOver(false);
+      };
+
+      let cancel = [
+        on(element, 'dragover', onDragOver),
+        on(element, 'dragleave', onDragLeave),
+        on(element, 'drop', onDrop),
+        on(window, 'dragover', onWindowDragOver),
+      ];
+
+      return () => {
+        cancel.forEach((c) => c());
+      };
+    };
+  };
+
+  const createHandleDropModel = (
+    _opts: OptionsInput<{
+      onDrop: (files: File[]) => void;
+      accept: string[];
+    }>,
+  ) => {
+    const opts = options(_opts);
+    let accept = $derived(opts.accept);
+    let isOver = $state(false);
+    let setOver = (over: boolean) => {
+      isOver = over;
+    };
+    return options({
+      accept: getter(() => accept),
+      isOver: getter(() => isOver),
+      setOver,
+      onDrop: (files: File[]) => opts.onDrop(files),
+    });
+  };
+
+  type FileDropModel = ReturnType<typeof createHandleDropModel>;
+
+  let model = createHandleDropModel({
+    accept: images,
+    onDrop: (files) => {
+      console.log('onDrop', files);
+    },
   });
+
+  $effect(() => console.log(model.isOver));
 </script>
 
 <div class="page">
-  <Form>
-    <Fields field={fields.record.role} />
-  </Form>
+  <div class="block" {@attach fileDrop(model)}></div>
 </div>
 
 <style lang="scss">
   .page {
-    padding: 50px;
-    display: flex;
-    flex-direction: column;
-    gap: 50px;
+    padding: 10px;
+    > .block {
+      width: 100px;
+      height: 100px;
+      border: 1px solid #000;
+      border-radius: 4px;
+    }
   }
 </style>
